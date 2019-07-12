e22a374714daec7f789b99134047dc9c058d8341
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-07-12 13:52:29 -0600

    Update coaching.py

--- coaching/coaching.py
+++ coaching/coaching.py
@@ -7,4 +7,30 @@ from redbot.core import Config, checks
 STUDENT_ROLES = []
 COACH_ROLES = []
 
-class coaching:
+class Coaching:
+  """Coaching Request Cog"""
+  
+  def __init__(self, bot):
+    self.bot = bot
+  
+  @commands.command()
+  async def setcoachrole(self, ctx):
+    await ctx.send("What roles are coaches:")
+  
+  
+  @commands.command(pass_context=True)
+  async def coachreq(self, ctx):
+    
+    student = ctx.message.author
+    
+    msg_coaches = ("**Coaching Request:** \n"
+                   "**Discord Name:** {}\n"
+                   "**IGN:** {} \n"
+                   "**Archetype / Deck to learn:** {}\n"
+                   "**Timezone:** {} \n"
+                   "**Time Avaliable:** {} \n"
+                   "**Additional Information** {} \n"
+                   "**Player Profile:**)
+    
+             
+                   
