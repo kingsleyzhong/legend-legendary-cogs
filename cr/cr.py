ca9f988c71ce07279b697125cf16afa90c5632d1
milan_25 <milan.abraham2@seznam.cz>
2019-07-12 22:04:16 +0200

    Added save tag command

--- cr/cr.py
+++ cr/cr.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands, Config, checks
 import clashroyale
+import os
 
 class ClashRoyaleCog(commands.Cog):
 
@@ -9,10 +10,42 @@ class ClashRoyaleCog(commands.Cog):
         self.config = Config.get_conf(self, identifier=2512325)
         default_user = {"tag" : None}
         self.config.register_user(**default_user)
+        os.getenv("CRAPI_TOKEN")
+        self.crapi = clashroyale.OfficialAPI(os.getenv("CRAPI_TOKEN"), is_async=True)
+        
+    def badEmbed(self, text):
+        bembed = discord.Embed(color=0xff0000)
+        bembed.set_author(name=text, icon_url="https://i.imgur.com/FcFoynt.png")
+        return bembed
+        
+    def goodEmbed(self, text):
+        gembed = discord.Embed(color=0x45cafc)
+        gembed.set_author(name=text, icon_url="https://i.imgur.com/qYmbGK6.png")
+        return gembed        
 
     @commands.command()
-    async def save(self, ctx, crtag):
-        await self.config.user(ctx.author).tag.set(crtag)
+    async def save(self, ctx, tag, member: discord.Member = None):
+        """Save your Clash Royale player tag"""
+        if member == None:
+            member = ctx.author        
+        
+        tag = tag.lower().replace('O', '0')
+        if tag.startswith("#"):
+            tag = tag.strip('#')
+
+        try:
+            player = await self.crapi.get_player("#" + tag)
+            await self.config.user(ctx.author).tag.set(tag)
+            await ctx.send(embed = self.goodEmbed("CR account {} was saved to {}".format(player.name, member.name)))
+            
+        except clashroyale.NotFoundError as e:
+            await ctx.send(embed = self.badEmbed("No player with this tag found, try again!"))
+
+        except clashroyale.RequestError as e:
+            await ctx.send(embed = self.badEmbed(f"CR API is offline, please try again later! ({str(e)})"))
+        
+        except Exception as e:
+            await ctx.send(embed = self.badEmbed("Something went wrong, this is unusual and shouldn't happen. Please message the bot to report this error."))
 
     @commands.command()
     async def get(self, ctx):
