d826273c005095bb21668a9927506e83aa74da92
milan_25 <milan.abraham2@seznam.cz>
2019-07-19 10:47:33 +0200

    added save and profile(wip)

--- bs/bs.py
+++ bs/bs.py
@@ -26,3 +26,85 @@ class BrawlStarsCog(commands.Cog):
         gembed.set_author(name=text, icon_url="https://i.imgur.com/qYmbGK6.png")
         return gembed        
 
+    @commands.command()
+    async def bssave(self, ctx, tag, member: discord.Member = None):
+        """Save your Brawl Stars player tag"""
+        if member == None:
+            member = ctx.author        
+        
+        tag = tag.lower().replace('O', '0')
+        if tag.startswith("#"):
+            tag = tag.strip('#')
+
+        try:
+            player = await self.bsapi.get_player(tag)
+            await self.config.user(member).tag.set(tag)
+            await ctx.send(embed = self.goodEmbed("BS account {} was saved to {}".format(player.name, member.name)))
+            
+        except brawlstats.errors.NotFoundError:
+            await ctx.send(embed = self.badEmbed("No player with this tag found, try again!"))
+
+        except brawlstats.errors.RequestError as e:
+            await ctx.send(embed = self.badEmbed(f"BS API is offline, please try again later! ({str(e)})"))
+        
+        except Exception as e:
+            await ctx.send("**Something went wrong, please send a personal message to <@590906101554348053> or try again!**")
+            
+    @commands.command(aliases=['bsp'])
+    async def bsprofile(self, ctx, member=None):
+        """Brawl Stars profile"""
+        await ctx.trigger_typing()
+        prefix = "/"
+        tag = ""
+
+        member = ctx.author if member is None else member
+
+        if isinstance(member, discord.Member):
+            tag = await self.config.user(member).tag()
+            if tag is None:
+                return await ctx.send(embed = self.badEmbed(f"This user has no tag saved! Use {prefix}bssave <tag>"))
+        elif isinstance(member, str) and member.startswith("<"):
+            id = member.replace("<", "").replace(">", "").replace("@", "").replace("!", "")
+            try:
+                member = discord.utils.get(ctx.guild.members, id=int(id))
+                if member is not None:
+                    tag = await self.config.user(member).tag()
+                    if tag is None:
+                        return await ctx.send(embed = self.badEmbed(f"This user has no tag saved! Use {prefix}bssave <tag>"))
+            except ValueError:
+                pass
+        elif isinstance(member, str) and member.startswith("#"):
+            tag = member.upper().replace('O', '0')
+        elif isinstance(member, str):
+            try:
+                member = discord.utils.get(ctx.guild.members, id=int(member))
+                if member is not None:
+                    tag = await self.config.user(member).tag()
+                    if tag is None:
+                        return await ctx.send(embed = self.badEmbed(f"This user has no tag saved! Use {prefix}bssave <tag>"))
+            except ValueError:
+                member = discord.utils.get(ctx.guild.members, name=member)
+                if member is not None:
+                    tag = await self.config.user(member).tag()
+                    if tag is None:
+                        return await ctx.send(embed = self.badEmbed(f"This user has no tag saved! Use {prefix}bssave <tag>"))
+
+        if tag is None or tag == "":
+            desc = "/bsprofile\n/bsprofile @user\n/bsprofile discord_name\n/bsprofile discord_id\n/bsprofile #CRTAG"
+            embed = discord.Embed(title="Invalid argument!", colour=discord.Colour.red(), description=desc)
+            return await ctx.send(embed=embed)
+        try:
+            player = await await self.bsapi.get_player(tag)
+            
+        except brawlstats.errors.NotFoundError:
+            return await ctx.send(embed = self.badEmbed("No clan with this tag found, try again!"))
+
+        except brawlstats.errors.RequestError as e:
+            return await ctx.send(embed = self.badEmbed(f"BS API is offline, please try again later! ({str(e)})"))
+        
+        except Exception as e:
+            return await ctx.send("**Something went wrong, please send a personal message to <@590906101554348053> or try again!**")
+
+        embed=discord.Embed(color=discord.Colour.blue())
+        embed.set_author(name=f"{player.name} {player.tag}", icon_url="https://i.imgur.com/40U8PnF.png")
+        await ctx.send(embed=embed)
