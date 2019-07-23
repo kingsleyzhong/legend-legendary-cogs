92a368e4a95a07fae5de71e195ecb7082df26a8b
milan_25 <milan.abraham2@seznam.cz>
2019-07-23 11:08:36 +0200

    added  postjob command

--- tools/tools.py
+++ tools/tools.py
@@ -1,6 +1,7 @@
 import discord
 from redbot.core import commands, Config, checks
 from discord.ext import tasks
+from datetime import datetime
 
 class Tools(commands.Cog):
     
@@ -64,4 +65,42 @@ class Tools(commands.Cog):
 
         await self.config.countdowns.set_raw(countdownMessage.id, value={"left" : seconds, "channel" : ctx.channel.id})
         await ctx.message.delete(delay=10)
+        
+    @commands.command()
+    async def postjob(self, ctx):
+        author = ctx.author
+        intro = await ctx.send("Please head over to a DM with me to answer some questions.")
+        await ctx.message.delete(delay=10)
+        await intro.delete(delay=10)
+        
+        def check(msg):
+            return msg.channel == author.dm_channel
+        
+        await author.send("Job posting/position opening:")
+        job = (await client.wait_for('message', check=check)).content
+        await author.send("Completion deadline:")
+        deadline = (await client.wait_for('message', check=check)).content
+        await author.send("Availability:")
+        availability = (await client.wait_for('message', check=check)).content
+        await author.send("How to contact:")
+        contact = (await client.wait_for('message', check=check)).content
+        await author.send("Job description:")
+        jobdesc = (await client.wait_for('message', check=check)).content
+        
+        embed=discord.Embed(colour=discord.Colour.green())
+        embed.add_field(name=job, value="\u200b")
+        embed.add_field(name="Posted by:", value=f"{author.mention} ({author.top_role})")
+        embed.add_field(name="Date:", value=datetime.now().strftime('%d %b %Y'))
+        embed.add_field(name="Completion deadline:", value=deadline)
+        embed.add_field(name="Availability:", value=availability)
+        embed.add_field(name="How to contact:", value=contact)
+        embed.add_field(name="Job description:", value=jobdesc)
+        
+        jobChannel = self.bot.get_channel(599320984675156020)
+        await jobChannel.send(embed=embed)
+        
+        
+        
+        
+        
 
