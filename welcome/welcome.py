6113a8f0b33bac46de7654ab348374e25030c6a5
milan_25 <milan.abraham2@seznam.cz>
2019-07-24 22:03:36 +0200

    enabled lafc setup

--- welcome/welcome.py
+++ welcome/welcome.py
@@ -17,11 +17,18 @@ class Welcome(commands.Cog):
     async def on_member_join(self, member):
         if member.guild.id == 440960893916807188 and not member.bot:
             await self.do_setup(member, new = True)
+        if member.guild.id == 593248015729295360 and not member.bot:
+            await self.do_setup_LAFC(member, new = True)
     
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         if member.guild.id == 440960893916807188 and not member.bot:
             welcomeCategory = discord.utils.get(member.guild.categories, id=598437481775497216)
+            for ch in welcomeCategory.channels:
+                if ch.topic == str(member.id):
+                    await ch.delete(reason="User left.")
+        if member.guild.id == 593248015729295360 and not member.bot:
+            welcomeCategory = discord.utils.get(member.guild.categories, id=602906519100719115)
             for ch in welcomeCategory.channels:
                 if ch.topic == str(member.id):
                     await ch.delete(reason="User left.")
