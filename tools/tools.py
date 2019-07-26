376eef02392fdb75832a52a80bdb759c14d28aa9
milan_25 <milan.abraham2@seznam.cz>
2019-07-26 21:29:43 +0200

    added error handling

--- tools/tools.py
+++ tools/tools.py
@@ -19,15 +19,18 @@ class Tools(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg):
         if msg.channel.id == 599320984675156020 and not msg.author.bot:
-            number = int(msg.content.split(" ")[0])
-            history = await msg.channel.history(limit=2).flatten()
-            numberPrev = int(history[1].content.split(" ")[0])
-            if number != numberPrev + 1:
-                await msg.channel.send(f"Wrong number! (Hint: {numberPrev} + 1)", delete_after=3)
-                return await msg.delete()
-            if msg.author == history[1].author:
-                await msg.channel.send(f"Slow down! Let other people count as well!", delete_after=3)
-                return await msg.delete()
+            try:
+                number = int(msg.content.split(" ")[0])
+                history = await msg.channel.history(limit=2).flatten()
+                numberPrev = int(history[1].content.split(" ")[0])
+                if number != numberPrev + 1:
+                    await msg.channel.send(f"Wrong number! (Hint: {numberPrev} + 1)", delete_after=3)
+                    return await msg.delete()
+                if msg.author == history[1].author:
+                    await msg.channel.send(f"Slow down! Let other people count as well!", delete_after=3)
+                    return await msg.delete()
+            except ValueError:
+                await msg.delete()
                 
 
     def convertToLeft(self, sec):
