f3780f0bc322bf3a15a0d8db33c828fcb0047816
milan_25 <milan.abraham2@seznam.cz>
2019-07-26 21:06:37 +0200

    Update tools.py

--- tools/tools.py
+++ tools/tools.py
@@ -22,7 +22,7 @@ class Tools(commands.Cog):
             print(msg.content)
             number = int(msg.content.split(" ")[0])
             history = await msg.channel.history(limit=2).flatten()
-            numberPrev = int(history[1])
+            numberPrev = int(history[1].content)
             await msg.channel.send(f"prev:{numberPrev} now:{number}")
 
     def convertToLeft(self, sec):
