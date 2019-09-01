98aab4f36c23c387d400870c6a77495535a80109
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-08-31 19:27:48 -0600

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -8,5 +8,16 @@ class HWCog(commands.Cog):
     """Prints hello world"""
     await ctx.send("Hello World")
 
-  
+  @commands.command(aliases=['ripgupta'])
+  async def ripgupta(self, ctx, count, *, message):
+    """Put in a sizeable number and put a message afterward"""
+    int(count)
+    gupta = 468209010978455552
+    channel = 617525238392946699
+    mloop = 0
+        
+    while mloop > count:
+      await channel.send("{} {}".format(gupta.mention, message))
+      int(mloop)
+      mloop = mloop + 1
   
